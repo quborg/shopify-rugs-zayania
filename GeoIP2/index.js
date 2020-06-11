1489783417eef91015b02769f684da814a29a5bd
quborg <miloud.laftah@gmail.com>
2020-06-12 00:32:17 +0100

    save

--- GeoIP2/index.js
+++ GeoIP2/index.js
@@ -8,27 +8,24 @@
 //   console.log(response.country.isoCode); // 'CA'
 // });
 
-const Reader = require('@maxmind/geoip2-node').Reader;
+// let Reader = require('@maxmind/geoip2-node').Reader;
 // Typescript:
-// import { Reader } from '@maxmind/geoip2-node';
+import { Reader } from '@maxmind/geoip2-node';
+
  
-const options = {
-  // you can use options like `cache` or `watchForUpdates`
-};
- 
-Reader.open('/usr/local/database.mmdb', options).then(reader => {
+Reader.open('./products-swift-bkp.mmdb').then(reader => {
   console.log(reader.country('1.1.1.1'));
 });
 
 
-const Reader = require('@maxmind/geoip2-node').Reader;
-// Typescript:
-// import { Reader } from '@maxmind/geoip2-node';
+// const Reader = require('@maxmind/geoip2-node').Reader;
+// // Typescript:
+// // import { Reader } from '@maxmind/geoip2-node';
  
-Reader.open('/usr/local/share/GeoIP/GeoIP2-City.mmdb').then(reader => {
-  const response = reader.city('128.101.101.101');
+// Reader.open('/usr/local/share/GeoIP/GeoIP2-City.mmdb').then(reader => {
+//   const response = reader.city('128.101.101.101');
  
-  console.log(response.country.isoCode); // 'US'
-  console.log(response.city.names.en); // 'Minneapolis'
-  console.log(response.postal.code); // '55407'
-});
+//   console.log(response.country.isoCode); // 'US'
+//   console.log(response.city.names.en); // 'Minneapolis'
+//   console.log(response.postal.code); // '55407'
+// });
---
+++ GeoIP2/webpack.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  // mode: "development",
+  // devtool: "inline-source-map",
+  // watch: true,
+  entry: {
+    "asset-vendor": "./index.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "assets"),
+  },
+  node: {
+    fs: "empty",
+  },
+};
